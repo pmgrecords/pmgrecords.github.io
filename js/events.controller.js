da71b34114824a21d150ca609199f15b003a563d
Alex Pepper <alex.pepper@harlandclarke.com>
2016-04-23 09:10:28 -0500

    updates

--- js/events.controller.js
+++ js/events.controller.js
@@ -9,6 +9,7 @@
         eventsService.getEvents()
             .then(function(events) {
                 vm.events = events;
+                vm.events.unshift({ imagePath: 'images/events/3.jpg'});
             });
     }
 })();
